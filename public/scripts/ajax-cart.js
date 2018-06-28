571c77a1a47a3666085d45dc9db943100dcf6e67
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-28 23:42:11 +0000

    Build out cart DOM element and add event handler so that when users click outside of cart, it is removed from DOM

--- public/scripts/ajax-cart.js
+++ public/scripts/ajax-cart.js
@@ -1,15 +1,58 @@
 console.log("hey there")
 
+function cartCancelHandler(){
+
+};
+
+function cartSubmitHandler(){
+
+};
+
+function createDOMCart(itemArr){
+  let $cart = $(`
+    <div class="cart-container">
+      <header>
+        <h1>
+          Checkout
+        </h1>
+      </header>
+      <section>
+        <table>
+          <tr>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Amount</th>
+          </tr>
+        </table>
+      </section>
+      <footer>
+        <button id="confirm-order">Confirm Order</button>
+      </footer>
+    </div>
+  `);
+  let $mask = $(`<div class="page-mask"></div>`);
+  let $body = $("body");
+  $body.prepend($mask);
+  $body.prepend($cart);
+};
+
+function destroyDOMCart(){
+  $(".cart-container").remove();
+  $(".page-mask").remove();
+};
+
 function cartClickHandler(event){
   $.ajax({
     type: "GET",
     url: "/cart",
     success: function(data){
       console.log(data);
+      createDOMCart();
+      $(".page-mask").on("click", destroyDOMCart);
     }
   })
-}
+};
 
 $(document).ready(function(){
   $(".cart-button").on("click", cartClickHandler);
-})
+});
