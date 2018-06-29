b1d57d2d50c24b11a3fc8be2e89438ecd277d767
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-29 14:56:04 +0000

    Move queries to new directory in ./db

---
+++ db/queries/queries.js
@@ -0,0 +1,28 @@
+const settings = require('../settings')
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+knex('logins').insert({
+  // email: req.body.email,
+  // password: bcrypt.hashSync(req.body.password, 10),
+}).then(insertCustomer).asCallback()
+
+function insertCustomer(arguments){
+ return knex('logins').select().then (result => {
+    knex('customers').insert({
+      // first_name: req.body.first_name,
+      // last_name: req.body.last_name,
+      // phone_number: req.body.phone_number,
+      // login_id: result[result.length - 1].login_id
+    }).asCallback()
+  })
+}
+
+module.exports = {}
--- server.js
+++ server.js
@@ -8,16 +8,18 @@ const nodeSassMiddleware = require("node-sass-middleware");
 const bcrypt = require("bcrypt")
 
 const pg = require("pg");
-const settings = require('./db/settings')
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host : settings.hostname,
-    user : settings.user,
-    password : settings.password,
-    database : settings.database
-  }
-});
+// const settings = require('./db/settings')
+// const knex = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host : settings.hostname,
+//     user : settings.user,
+//     password : settings.password,
+//     database : settings.database
+//   }
+// });
+const queries = require("./db/queries/queries.js");
+
 const mockDB = {
   users:{},
   restaurants:{
@@ -270,21 +272,6 @@ app.post("/signup", (req, res) => {
     //   last_name: req.body.last_name,
     //   phone_number: req.body.phone_number,
     // }).asCallback()
-    knex('logins').insert({
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 10),
-    }).then(insertCustomer).asCallback()
-
-    function insertCustomer(){
-     return knex('logins').select().then (result => {
-        knex('customers').insert({
-          first_name: req.body.first_name,
-          last_name: req.body.last_name,
-          phone_number: req.body.phone_number,
-          login_id: result[result.length - 1].login_id
-        }).asCallback()
-      })
-    }
 
     req.session.email = req.body.email;
     req.session.first_name = req.body.first_name;
