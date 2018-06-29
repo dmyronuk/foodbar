cd7ffcc8b7db01b0392c01fb737d62d032b493aa
Adib <adibalamir@hotmail.com>
2018-06-29 15:37:44 +0000

    updated queries

--- db/queries/queries.js
+++ db/queries/queries.js
@@ -9,21 +9,25 @@ const knex = require('knex')({
   }
 });
 
-knex('logins').insert({
-  // email: req.body.email,
-  // password: bcrypt.hashSync(req.body.password, 10),
-}).then(insertCustomer).asCallback()
+function insertLogin(obj) {
+  knex("login").insert({
+    "email": obj.email,//req.body.email,
+    "password": obj.password,//bcrypt.hashSync(req.body.password, 10),
+  }).then(insertCustomer).asCallback()
+}
 
-function insertCustomer(arguments){
- return knex('logins').select().then (result => {
-    knex('customers').insert({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      phone_number: req.body.phone_number,
-      login_id: result[result.length - 1].login_id
+function insertCustomer(obj){
+ return knex("logins").select().then (result => {
+    knex("customer").insert({
+      first_name: obj.first_name//req.body.first_name,
+      last_name: obj.last_name//req.body.last_name,
+      phone_number: obj.phone_number//req.body.phone_number,
+      login_id: obj.login_id//result[result.length - 1].login_id
     }).asCallback()
   })
 }
+
+knex("customer").join("order", "order.customer id", "=", "customer.customer_id").join()
 
 module.exports = {}
 
